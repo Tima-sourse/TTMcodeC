8ccdc863d223d948e6f7d60e9086c6e56e646d10
Tima-sourse <53928588+Tima-sourse@users.noreply.github.com>
2024-02-15 09:39:32 +1000

    Update lib1.c

--- lib1.c
+++ lib1.c
@@ -20,15 +20,20 @@ void main()
         d = pow(b,2) - 4*a*c;
     
         if (d < 0) 
-            {printf("\nD < 0\nкоренй нет");}
+        {
+            printf("\nD < 0\nкоренй нет");
+        }
         if (d > 0) 
-            {x1=((-b+sqrt(d))/(2*a));
+        {
+            x1=((-b+sqrt(d))/(2*a));
             x2=((-b-sqrt(d))/(2*a));
-            printf("\nD > 0\nx1 = %lf x2 = %lf", x1,x2);}
+            printf("\nD > 0\nx1 = %lf x2 = %lf", x1,x2);
+        }
         if (d == 0) 
-            {x=((-b)/(2*a));
-            printf("\nD=0\nx = %lf",x);}
-         
+        {
+            x=((-b)/(2*a));
+            printf("\nD=0\nx = %lf",x);
+        }
         printf("\nЧтобы закрыть программу введите 0, или любую другую для продолжения\n>>");
         scanf("%d", &end); 
     }
