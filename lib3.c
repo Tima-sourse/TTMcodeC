a602b69cb8a648abde1c21a6dc36a85dec32b68e
Тим <53928588+Tima-sourse@users.noreply.github.com>
2024-03-06 18:55:36 +1000

    Update lib3.c

--- lib3.c
+++ lib3.c
@@ -1,28 +1,30 @@
 #include <stdio.h>
 #include <windows.h>
 #include <string.h>
-#include <conio.h>
 
 void main()
 {   
     SetConsoleOutputCP(CP_UTF8);
-    int i, j;
+    int i, j, end=1;
     char str[81], a[2]="aA", b[2]="bB";
     
-    
-    printf("Введите строку - ");
-    scanf("%s", &str);
-    for (i = 0; i < strlen(str) ; i++)
+    while (end != 0)
     {
-        if (str[i] == a[0])
+        printf("Введите строку - ");
+        scanf("%s", &str);
+        for (i = 0; i < strlen(str) ; i++)
         {
-            str[i] = a[1];
+            if (str[i] == a[0])
+            {
+                str[i] = a[1];
+            }
+            if (str[i] == b[0])
+            {
+                str[i] = b[1];
+            }
         }
-        if (str[i] == b[0])
-        {
-            str[i] = b[1];
-        }
+        printf("\nСтрока с заменой - %s\n", str);
+        printf("\n\nЧто сделать: 0-закрыть программу | любое другая цифра-повторить\n>> ");
+        scanf("%d", &end);
     }
-    printf("\nСтрока с заменой - %s", str);
-    _getch();
 }
