2a0fdeb39600f179436b4a48f248027511fb9e29
Tima-sourse <53928588+Tima-sourse@users.noreply.github.com>
2024-03-06 21:45:44 +1000

    Create lib1.c

---
+++ lib1/lib1.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <math.h>
+#include <windows.h>
+void main() 
+{   
+    SetConsoleOutputCP(CP_UTF8);
+    double a, b, c, x, x1, x2, d;
+    int end=1;
+    
+    
+    while (end!=0)
+    {
+        printf("Введите a: ");
+        scanf("%lf", &a);
+        printf("\nВведите b: ");
+        scanf("%lf", &b);
+        printf("\nвведите c: ");
+        scanf("%lf", &c);
+    
+        d = pow(b,2) - 4*a*c;
+    
+        if (d < 0) 
+        {
+            printf("\nD < 0\nкоренй нет");
+        }
+        if (d > 0) 
+        {
+            x1=((-b+sqrt(d))/(2*a));
+            x2=((-b-sqrt(d))/(2*a));
+            printf("\nD > 0\nx1 = %lf x2 = %lf", x1,x2);
+        }
+        if (d == 0) 
+        {
+            x=((-b)/(2*a));
+            printf("\nD=0\nx = %lf",x);
+        }
+        printf("\nЧтобы закрыть программу введите 0, или любую другую для продолжения\n>>");
+        scanf("%d", &end); 
+    }
+}
