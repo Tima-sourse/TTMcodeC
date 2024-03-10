62921ecf90c25360168b60042e3081c4e8f14189
Tima-sourse <53928588+Tima-sourse@users.noreply.github.com>
2024-03-10 13:25:51 +1000

    Create fun.c

---
+++ lib4/fun.c
@@ -0,0 +1,13 @@
+#include <math.h>
+#include "fun.h"
+
+double math1(double a, double b, double c)
+{
+    return (a + b + c);
+}
+
+double math2(double a, double b, double c)
+{
+    double p = ((a + b + c) / 2);
+    return (sqrt(p*(p-a)*(p-b)*(p-c)));
+}
