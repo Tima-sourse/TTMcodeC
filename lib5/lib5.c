836c8e92bd2c40c1b8ce89ebce52a93f3e673676
Tima-sourse <53928588+Tima-sourse@users.noreply.github.com>
2024-03-10 18:02:06 +1000

    Create task5.c

--- lib5/lib5.c
+++ lib5/lib5.c
@@ -1,18 +1,28 @@
 #include <stdio.h>
 #include <malloc.h>
+#include <windows.h>
+#include "act.c"
+#include <stdlib.h>
 
 int main()
 {
+    SetConsoleOutputCP(CP_UTF8);
     double* a;
     double* b;
     int i, j, n;
+    char act;
     printf("Введите порядок матриц: ");
     scanf("%d", &n);
     printf("\n");
     
     a = (double*)malloc(n * n * sizeof(double));
     b = (double*)malloc(n * n * sizeof(double));
-    
+    if (a == NULL || b == NULL) 
+    {
+        printf("Ошибка выделения памяти\n");
+        return 0;
+    }
+
     printf("Введите элементы матрицы A построчно:\n");
     for (i = 0; i < n; i++)
     {
@@ -55,7 +65,56 @@ int main()
     printf("\n");
     }
     
+    printf("\nВведите знак операции |+|-|*|:\n");
+    scanf("%c", &act);
+    double *res;
+    switch (act)
+    {
+        case '+':
+            res = sum(n, a, b); 
+            printf("Сумма матриц:\n");
+            for (i = 0; i < n; i++) 
+            {
+                for (j = 0; j < n; j++) 
+                {
+                    printf(" %5.2lf |", *(a + i * n + j));
+                }
+                printf("\n");
+            }
+            break;
+        case '-':
+            res = diff(n, a, b);
+            printf("Разность матриц:\n");
+            for (i = 0; i < n; i++)
+            {
+                for (j = 0; j < n; j++)
+                {
+                    printf(" %5.2lf |", *(a + i * n + j));
+                }
+                printf("\n");
+            }
+            break;
+        case '*':
+            res = mult(n, a, b);
+            printf("Произведение матриц:\n");
+            for(i = 0; i < n; i++)
+            {
+                for(j = 0; j < n; j++)
+                {
+                    printf(" %5.2lf |", *(a + i * n + j));
+                }
+                printf("\n");
+            } 
+            break;
+        default: 
+            printf("Операция введена неправильно\n"); 
+            break;
+    }
+
     free(a);
-    getchar();  getchar();
+    free(b);
+    free(res);
+
+    getchar();
     return 0;
 }
