decd0fa30b5802b99fc4ef5aa2adb922bff793d2
Тим <53928588+Tima-sourse@users.noreply.github.com>
2024-03-10 15:28:08 +1000

    Update lib5.c

--- lib5/lib5.c
+++ lib5/lib5.c
@@ -4,13 +4,16 @@
 int main()
 {
     double* a;
+    double* b;
     int i, j, n;
-    printf("Введите порядок матрицы - ");
+    printf("Введите порядок матриц: ");
     scanf("%d", &n);
+    printf("\n");
     
     a = (double*)malloc(n * n * sizeof(double));
-  
-    printf("Введите элементы матрицы построчно:\n");
+    b = (double*)malloc(n * n * sizeof(double));
+    
+    printf("Введите элементы матрицы A построчно:\n");
     for (i = 0; i < n; i++)
     {
         for (j = 0; j < n; j++)
@@ -20,7 +23,28 @@ int main()
     printf("--\n");
     }
   
-    printf("\nМатрица:\n");
+    printf("\nМатрица A:\n");
+    for (i = 0; i < n; i++)
+    {   
+        printf("|");
+        for (j = 0; j < n; j++)
+        {
+            printf(" %5.2lf |", *(a + i * n + j));
+        }
+    printf("\n");
+    }
+    
+    printf("\nВведите элементы матрицы B построчно:\n");
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            scanf("%lf", (a + i * n + j));
+        }
+    printf("--\n");
+    }
+  
+    printf("\nМатрица B:\n");
     for (i = 0; i < n; i++)
     {   
         printf("|");
