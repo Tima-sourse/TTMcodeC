9cb3576ba67e85346ccbd69fc680dfefb3ca1d29
Tima-sourse <53928588+Tima-sourse@users.noreply.github.com>
2024-03-10 20:41:23 +1000

    Update act.c

--- lib5/act.c
+++ lib5/act.c
@@ -1,41 +1,53 @@
 #include <malloc.h>
 
-double* sum(int n, double* a, double* b)
+double** sum(int n, double **a, double **b)
 {
-    double* sum = (double*)malloc(n * n * sizeof(double));
+    double **sum = (double **)malloc(n * sizeof(double));
+    for(int i = 0; i < n; i++)
+    {
+        sum[i] = (double *)malloc(n*sizeof(double));
+    }
     for(int i = 0; i < n; i++)
     {
         for(int j = 0; j < n; j++)
         {
-            *(sum + i * n + j) = *(a + i * n + j) + *(b + i * n + j);
+            sum[i][j] = a[i][j] + b[i][j];
         }
     }
     return sum;
 }
 
-double* diff(int n, double* a, double* b)
+double** diff(int n, double **a, double **b)
 {
-    double* diff = (double*)malloc(n * n * sizeof(double));
+    double **diff = (double **)malloc(n * sizeof(double));
+    for(int i = 0; i < n; i++)
+    {
+        diff[i] = (double *)malloc(n*sizeof(double));
+    }
     for(int i = 0; i < n; i++)
     {
         for(int j = 0; j < n; j++)
         {
-            *(diff + i * n + j) = *(a + i * n + j) - *(b + i * n + j);
+            diff[i][j] = a[i][j] - b[i][j];
         }
     }
     return diff;
 }
-double* mult(int n, double *a, double *b)
+double** mult(int n, double **a, double **b)
 {
-    double *mult = (double *)malloc(n * n * sizeof(double));
+    double **mult = (double **)malloc(n * sizeof(double));
+    for(int i = 0; i < n; i++)
+    {
+        mult[i] = (double *)malloc(n*sizeof(double));
+    }
     for(int i = 0; i < n; i++)
     {
         for(int j = 0; j < n; j++)
         {
-            
+            mult[i][j] = 0;
             for(int k = 0; k < n; k++)
             {
-                *(mult + i * n + j) += (*(a + i * n + j)) * (*(b + i * n + j));
+                mult[i][j] += a[i][k] * b[k][j];
             }
         }
     }
