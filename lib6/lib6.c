c11e88d8648a5a126d26442a39ed7121c5156268
Tima-sourse <53928588+Tima-sourse@users.noreply.github.com>
2024-03-17 14:10:38 +1000

    Update lib6.c

--- lib6/lib6.c
+++ lib6/lib6.c
@@ -7,21 +7,20 @@ int main()
     SetConsoleCP(CP_UTF8);
     int i, j;
     double limit_f;
-    char str[60], limit_s[4];
+    char str[64], limit_s[2];
     FILE *F_in = fopen("in.txt", "rt"), *F_out = fopen("out.txt", "wt");
 
-    while (fgets(str, 60, F_in) != NULL)
-    {
-        for (i = 0; i < 61; i++)
+    while (fgets(str, 64, F_in) != NULL)
+    {   
+        printf("%s", str);
+        for (i = 0; i < 65; i++)
         {
             if ((str[i] == '1') && (str[i + 1] == '9'))
             {   
-                limit_s[0] = str[i];
-                limit_s[1] = str[i + 1];
-                limit_s[2] = str[i + 2];
-                limit_s[3] = str[i + 3];
+                limit_s[0] = str[i + 2];
+                limit_s[1] = str[i + 3];
                 limit_f = atof(limit_s);
-                if (limit_f > 1980)
+                if (limit_f > 80)
                 {   
                     fputs(str, F_out);
                     printf("%s", str);
