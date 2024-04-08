cb4bf1c39f83f4e53e2103c36565e1b14684878c
Tima-sourse <53928588+Tima-sourse@users.noreply.github.com>
2024-04-08 17:16:47 +1000

    Update lib9.c
    подгруз и тестового файла

--- lib9/lib9.c
+++ lib9/lib9.c
@@ -2,40 +2,42 @@
 #include <windows.h>
 #include <stdlib.h>
 #include <string.h>
-float sum(char *num1, char *num2);
-float diff(char *num1, char *num2);
 
 int main()
 {
     SetConsoleOutputCP(CP_UTF8);
-    char str[100], num1[50], num2[50], operand;
-    float res;
+    char str[100], leksema_str[20], operand;
+    float res = 0;
+    char *leksema_p;
 
     printf("Введите вычисляемое выражение: ");
     fgets(str, sizeof(str), stdin);
-    sscanf(str, "%s %c %s", num1, &operand, num2);
     
+    leksema_p = strtok(str, " ");
+    strcpy(leksema_str, leksema_p);
+    res += atof(leksema_str);
 
-    if (operand == '+')
+    while (leksema_p != NULL)
     {
-        res = sum(num1, num2);
-    }
-    else
-    {
-        res = diff(num1, num2);
+        leksema_p = strtok(NULL, " ");
+        if (leksema_p != NULL)
+        {
+            strcpy(leksema_str, leksema_p);
+            if (strlen(leksema_str) == 1)
+            {
+                operand = leksema_str[0];
+            }
+            else if(operand == '+')
+            {
+                res += atof(leksema_str);
+            }
+            else if(operand == '-')
+            {
+                res -= atof(leksema_str);
+            }
+        }
     }
 
     printf("Результат вычисления: %.2f", res);
-
     return 0;
-}
-
-float sum(char *num1, char *num2)
-{   
-    return (atof(num1) + atof(num2));
-}
-
-float diff(char *num1, char *num2)
-{   
-    return (atof(num1) - atof(num2));
 }
