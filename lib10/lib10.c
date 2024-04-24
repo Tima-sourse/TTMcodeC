326423f0b395b7ad1d33a22b7faa431c507d716f
Tima-sourse <53928588+Tima-sourse@users.noreply.github.com>
2024-04-24 10:45:05 +1000

    Update lib10.c

--- lib10/lib10.c
+++ lib10/lib10.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_ARRAY(arr, len) ({ \
+#define MAX_NUM_ARR(arr, len) ({ \
     double _max = arr[0]; \
     for (int _i = 1; _i < len; _i++) { \
         if (arr[_i] > _max) { \
@@ -11,7 +11,8 @@
     _max; \
 })
 
-int main() {
+int main() 
+{
     double num_arr[50];
     int count = 0;
     char str[100];
@@ -32,7 +33,7 @@ int main() {
         str_p = stop;
     }
 
-    double max_num = MAX_ARRAY(num_arr, count);
+    double max_num = MAX_NUM_ARR(num_arr, count);
     printf("Максимальное число: %.2f\n", max_num);
 
     return 0;
