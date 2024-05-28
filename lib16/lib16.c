54d3462209e0f80a87372cc6f2d1d89e515feafb
Tima-sourse <53928588+Tima-sourse@users.noreply.github.com>
2024-05-29 01:29:17 +1000

    Update lib16.c

--- lib16/lib16.c
+++ lib16/lib16.c
@@ -2,6 +2,26 @@
 #include <stdio.h>
 #include <stdint.h>
 
+int F_long_num(long num);
+int F_double_num(double num);
+
+int main()
+{
+    long num_long;
+    printf("Введите число в формате LONG - ");
+    scanf("%ld", &num_long);
+    getchar();
+    printf("Кол-во единичных битов в LONG - %d", F_long_num(num_long));
+
+    double num_double;
+    printf("\n\nВведите число в формате DOUBLE - ");
+    scanf("%lf", &num_double);
+    getchar();
+    printf("\nКол-во единичных битов в DOUBLE - %d", F_double_num(num_double));
+
+    return 0;
+}
+
 int F_long_num(long num)
 {
     int res = 0;
@@ -23,21 +43,4 @@ int F_double_num(double num)
         num_p >>=1;
     }
     return res;
-}
-
-int main()
-{
-    long num_long;
-    printf("Введите число в формате LONG - ");
-    scanf("%ld", &num_long);
-    getchar();
-    printf("Кол-во единичных битов в LONG - %d", F_long_num(num_long));
-
-    double num_double;
-    printf("\n\nВведите число в формате DOUBLE - ");
-    scanf("%lf", &num_double);
-    getchar();
-    printf("\nКол-во единичных битов в DOUBLE - %d", F_double_num(num_double));
-
-    return 0;
 }
